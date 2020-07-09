338b975928990a471579dce8c7ee0332ee880ecf
Robert-Siberry <robert.siberry@outlook.com>
2020-07-09 21:09:37 +0100

    updated user and post tables

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request
-from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from wtforms import ValidationError
@@ -11,9 +10,7 @@ from flask_login import login_user, current_user, logout_user, login_required, U
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
                                         environ.get('MYSQL_USER') + \
                                         ':' + \
@@ -24,7 +21,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
                                         environ.get('MYSQL_PORT') + \
                                         '/' + \
                                         environ.get('MYSQL_DB_NAME')
-db = SQLAlchemy(app)
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
@@ -44,6 +40,7 @@ class eliquids(db.Model):
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
     flavours = db.Column(db.String(300), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
         return ''.join(
@@ -55,22 +52,19 @@ class eliquids(db.Model):
         )
 
 
-
-
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     f_name = db.Column(db.String(30), nullable=False)
     l_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(500), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
+    posts = db.relationship('eliquids', backref='author', lazy=True)
 
     def __repr__(self):
         return ''.join(['User ID: ', str(self.id), '\r\n',
                         'Email: ', self.email, '\r\n',
                         'Name: ', self.f_name, ' ', self.l_name
                         ])
-
-
 
 
 def validate_email(email):
@@ -80,23 +74,25 @@ def validate_email(email):
         raise ValidationError('Email already in use')
 
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        hash_pw = bcrypt.generate_password_hash(form.password.data)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    else:
+        if form.validate_on_submit():
+            hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(f_name=form.f_name.data,
-                     l_name=form.l_name.data,
-                     email=form.email.data,
-                     password=hash_pw
-                     )
+            user = Users(f_name=form.f_name.data,
+                         l_name=form.l_name.data,
+                         email=form.email.data,
+                         password=hash_pw
+                         )
 
-        db.session.add(user)
-        db.session.commit()
+            db.session.add(user)
+            db.session.commit()
 
-        return redirect(url_for('login'))
+            return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
@@ -147,7 +143,8 @@ def add():
             brand=form.brand.data,
             name=form.name.data,
             description=form.description.data,
-            flavours=form.flavours.data
+            flavours=form.flavours.data,
+            author=current_user
         )
         db.session.add(post_data)
         db.session.commit()
